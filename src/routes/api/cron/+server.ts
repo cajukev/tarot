7e53c2a0d9aecd2f4c2e2de47d22e6e665ae7302
cajukev <kcajukev@gmail.com>
2023-04-15 12:13:33 -0400

    Cron in try catch

--- src/routes/api/cron/+server.ts
+++ src/routes/api/cron/+server.ts
@@ -1,12 +1,17 @@
 import { dbSecret } from '$lib/db';
 
 export function GET() {
-  dbSecret.from("Profile")
-  .update({ tokens: 15})
-  .lt("tokens", 15)
-  .then((data) => {
-    console.log(data);
-  });
-    
-  return new Response('Hello Cron!');
+  try {
+    dbSecret.from("Profile")
+      .update({ tokens: 15})
+      .lt("tokens", 15)
+      .then((data) => {
+        console.log(data);
+      });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log("finally");
+    return new Response('Hello Cron!');
+  }
 }
