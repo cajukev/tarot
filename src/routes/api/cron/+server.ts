df71faee567cb1daba0bc3cc7e3c0438deffb6f6
cajukev <kcajukev@gmail.com>
2023-04-15 01:01:06 -0400

    Updated Cron

--- src/routes/api/cron/+server.ts
+++ src/routes/api/cron/+server.ts
@@ -1,6 +1,9 @@
 import { dbSecret } from '$lib/db';
 
 export function GET() {
-  dbSecret.rpc('cron_update_profile_tokens')
+  dbSecret.from('Profile')
+  .update({ tokens: 15 })
+  .lt('tokens', 15)
+    
   return new Response('Hello Cron!');
 }
