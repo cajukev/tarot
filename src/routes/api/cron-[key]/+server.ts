43672fdb844cdf70bbf1666b68bb9316afd24064
cajukev <kcajukev@gmail.com>
2023-04-15 12:40:49 -0400

    Cron security

---
+++ src/routes/api/cron-[key]/+server.ts
@@ -0,0 +1,17 @@
+import { dbSecret } from '$lib/db';
+
+export function GET({request, params}) {
+  if(params.key === import.meta.env.VITE_WEBHOOK_SHARED_KEY ) {
+    try {
+      dbSecret.from("Profile")
+        .update({ tokens: 15})
+        .lt("tokens", 15)
+    } catch (error) {
+      console.log(error);
+    } finally {
+      return new Response('Yes Cron!');
+    }
+  }else{
+    return new Response('No Cron!');
+  }
+}
