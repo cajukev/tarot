696b9bdffd6819782086f241603a428a2b809fbd
cajukev <kcajukev@gmail.com>
2023-04-15 13:12:00 -0400

    Working Cron-key update

--- src/routes/api/cron-[key]/+server.ts
+++ src/routes/api/cron-[key]/+server.ts
@@ -1,23 +1,19 @@
 import { dbSecret } from '$lib/db';
 
-export function GET({request, params}) {
-  if(params.key === import.meta.env.VITE_WEBHOOK_SHARED_KEY ) {
-    try {
-      let data = dbSecret.from("Profile")
-        .update({ tokens: 15})
-        .lt("tokens", 15)
-        .select()
-        .then((data) => {
-          return data;
-        });
-      return new Response('Yes Cron!');
-    } catch (error) {
-      console.log(error);
-      return new Response('No Cron!');
-    } finally {
-      return new Response('No Cron!');
+export async function GET({request, params}) {
+
+  if(params.key === import.meta.env.VITE_CRON_SHARED_KEY) {
+    let data = await dbSecret.from("Profile")
+      .update({ tokens: 15})
+      .lt("tokens", 15)
+      .select("id")
+      .then((data) => {
+        return data.data
+      });
+
+      return new Response(JSON.stringify(data));
     }
-  }else{
-    return new Response('No Cron!');
-  }
+    else {
+      return new Response("Invalid key", {status: 401});
+    }
 }
