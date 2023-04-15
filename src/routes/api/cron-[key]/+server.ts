ec68c92141af0424a89210a5fdde50be5fb71614
cajukev <kcajukev@gmail.com>
2023-04-15 12:56:41 -0400

    Update Cron with data

--- src/routes/api/cron-[key]/+server.ts
+++ src/routes/api/cron-[key]/+server.ts
@@ -3,14 +3,19 @@ import { dbSecret } from '$lib/db';
 export function GET({request, params}) {
   if(params.key === import.meta.env.VITE_WEBHOOK_SHARED_KEY ) {
     try {
-      dbSecret.from("Profile")
+      let data = dbSecret.from("Profile")
         .update({ tokens: 15})
         .lt("tokens", 15)
+        .select()
+        .then((data) => {
+          return data;
+        });
+      return new Response('Yes Cron!');
     } catch (error) {
       console.log(error);
       return new Response('No Cron!');
     } finally {
-      return new Response('Yes Cron!');
+      return new Response('No Cron!');
     }
   }else{
     return new Response('No Cron!');
