1d61ee77fc065b7ada87e8f109351c2d6656306a
cajukev <kcajukev@gmail.com>
2023-04-17 16:28:00 -0400

    Image optimization fix

--- src/routes/Collection.svelte
+++ src/routes/Collection.svelte
@@ -195,17 +195,6 @@
 		gap: 0.75rem;
 		.card {
 			width: max(60px, 8%);
-			.hiddenImg {
-				position: fixed;
-				top: 0;
-				left: 0;
-				width: 0;
-				height: 0;
-				overflow: hidden;
-				opacity: 0;
-				visibility: hidden;
-				pointer-events: none;
-			}
 			img {
 				border-radius: 0.25rem;
 				border: 2px solid white;
@@ -220,6 +209,17 @@
 					transform: translateY(-0.25rem);
 				}
 			}
+		}
+		.hiddenImg {
+			position: fixed;
+			top: 0;
+			left: 0;
+			width: 0;
+			height: 0;
+			overflow: hidden;
+			opacity: 0;
+			visibility: hidden;
+			pointer-events: none;
 		}
 	}
 
