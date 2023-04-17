9b77a9ae56613770d13f9389d980cc44b22107a4
cajukev <kcajukev@gmail.com>
2023-04-17 18:33:28 -0400

    Success page fix

--- src/routes/success/+page.svelte
+++ src/routes/success/+page.svelte
@@ -8,4 +8,18 @@
 </div>
 
 <style lang="scss">
+  .container{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 100vh;
+	}
+	a{
+		margin: 2rem;
+		color: rgba($color: white, $alpha: 0.7);
+	}
+  p {
+		margin-top: 2rem;
+		max-width: 20rem;
+	}
 </style>
