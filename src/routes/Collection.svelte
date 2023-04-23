21a01fbc144eeba92f0818da69ff932ff83437fb
cajukev <kcajukev@gmail.com>
2023-04-23 01:24:10 -0400

    collection fix

--- src/routes/Collection.svelte
+++ src/routes/Collection.svelte
@@ -28,16 +28,13 @@
 		if ($collectionStore?.length > 0) {
 			decks = $collectionStore;
 		}else{
-			console.log(decks, cards.entries())
 			decks = [];
 			for (let [key, deck] of cards.entries()) {
-				if (!decks.some((d) => d.name === deck.name)){
 					decks.push(deck);
 					deck.available = (!unlocks.get(deck.abbrv) || $page.data.profile?.data.experience >= (unlocks.get(deck.abbrv)?.exp || 0)) && (!secrets.has(deck.abbrv) || $page.data.profile?.data.secrets.includes(deck.abbrv) )
-				}
+				
 			}
 		}
-		console.log(decks);
 
 	}
 
