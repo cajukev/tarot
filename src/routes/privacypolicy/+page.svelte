fb453f8e279074d0059b698f3a55834c9d946b3e
cajukev <kcajukev@gmail.com>
2023-06-14 00:37:03 -0400

    Policy changes

--- src/routes/privacypolicy/+page.svelte
+++ src/routes/privacypolicy/+page.svelte
@@ -5,9 +5,10 @@
   <!-- Return -->
   <a href="/" class="return"><h4>&larr; Return</h4></a>
   <h2>Privacy Policy</h2>
-  <p>Last updated: 1/16/2023</p>
-
-  <li>This website is operated by Alex Brousseau, an individual</li>
+  <p>Last updated: 6/14/2023</p>
+  <li>This website is operated by Kevin Brousseau, an individual</li>
+  <li>This website is for entertainment purposes ONLY. ny readings given should not be taken as medical, financial, relationship, or legal advice.</li>
+  <li>All readings use the OpenAI API</li>
   <li>When you sign in, a cookie is stored on your device to keep you logged in. This cookie is stored on your device and is not sent to any server.</li>
   <li>When you sign in, your email is stored in a <a href="https://supabase.com/">Supabase</a> project. This email is used to identify you and to send you a login link when you request one.</li>
   <li>Payment is securely processed via <a href="https://stripe.com/en-ca">Stripe</a></li>
