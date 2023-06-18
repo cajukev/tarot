ad919947613866012a1d9b69d9f52876f8b9087b
cajukev <kcajukev@gmail.com>
2023-06-18 00:26:14 -0400

    Microphone cost

--- src/routes/api/speechToText/+server.ts
+++ src/routes/api/speechToText/+server.ts
@@ -13,6 +13,25 @@ export const POST: RequestHandler = async ({ request, locals, fetch }) => {
         audio: any;
     } = await request.json();
 
+    // Verify tokens
+    const profileData = await locals.sb.from('Profile')
+        .select('*')
+        .eq('id', locals.session.user.id)
+        .single()
+
+    if (profileData.data!.tokens < 0.01) {
+        return new Response(
+            JSON.stringify({
+                error: "Not enough tokens",
+            }),
+        );
+    }
+
+    await locals.sb.from('Profile')
+        .update({ tokens: profileData.data!.tokens - 0.01 })
+        .eq('id', locals.session.user.id)
+        .single()
+
     console.log(formData);
 
     const audio = formData.audio;
