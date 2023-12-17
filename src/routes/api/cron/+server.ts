8d6c8a7ea9ef17ae55f33e46bf2388a9a0523706
cajukev <kcajukev@gmail.com>
2023-12-16 23:49:17 -0500

    Add CRON Job

---
+++ src/routes/api/cron/+server.ts
@@ -0,0 +1,25 @@
+import { dbSecret } from '$lib/db';
+
+export async function GET(req) {
+
+  if (req.request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response(JSON.stringify({error: "Unauthorized"}), {status: 401});
+  }
+  let resetDaily = await dbSecret.from("Profile")
+    .update({ daily: false})
+    .eq("daily", true)
+    .select("id")
+    .then((data) => {
+      return data.data
+    });
+
+  let regenTokens = await dbSecret.from("Profile")
+    .update({ tokens: 30})
+    .lt("tokens", 30)
+    .select("id")
+    .then((data) => {
+      return data.data
+    });
+
+    return new Response(JSON.stringify({resetDaily, regenTokens}));
+}
